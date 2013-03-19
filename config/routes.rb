1c2aa50abbd37ecd21e0bf97a54dbf51e1c27409
Bill <roundedvision@gmail.com>
2013-03-19 14:02:16 -0700

    Assets are now strictly served through FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Ticketee::Application.routes.draw do
       resources :permissions
     end
   end
+
+  resources :files
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
