ec48a1fb16e1336afb6648c8cd25ba6467b978af
Bill <roundedvision@gmail.com>
2013-03-20 16:15:54 -0700

    Add Admin::StatesController for managing states

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -2,12 +2,11 @@ module TicketsHelper
   def state_for(comment)
     content_tag(:div, :class => "states") do
       if comment.state
-        previous_state = comment.previous_state
-          if previous_state && comment.state != previous_state
-            "#{render previous_state} &rarr; #{render comment.state}"
-          else
-            render(comment.state)
-          end
+        if comment.previous_state && comment.state != comment.previous_state
+          "#{render comment.previous_state} &rarr; #{render comment.state}".html_safe
+        else
+          render(comment.state)
+        end
       end
     end
   end
