01b40b6cd14e1e90452b6288b3403d18a1b42a77
Bill <roundedvision@gmail.com>
2013-03-24 13:21:57 -0700

    Users can tag tickets upon creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,8 +4,21 @@ class Ticket < ActiveRecord::Base
   belongs_to :state
   has_many :assets
   has_many :comments
+  has_and_belongs_to_many :tags
   accepts_nested_attributes_for :assets
-  attr_accessible :description, :title, :assets_attributes
+  attr_accessible :description, :title, :assets_attributes, :tag_names
+  attr_accessor :tag_names
+  before_create :associate_tags
+
   validates :title, :presence => true
   validates :description, :presence => true, :length => {:minimum => 10}
+
+  private
+  def associate_tags
+    if tag_names
+      tag_names.split(" ").each do |name|
+        self.tags << Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320232814) do
+ActiveRecord::Schema.define(:version => 20130324181310) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(:version => 20130320232814) do
     t.string  "color"
     t.string  "background"
     t.boolean "default",    :default => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "tags_tickets", :id => false, :force => true do |t|
+    t.integer "tag_id"
+    t.integer "ticket_id"
   end
 
   create_table "tickets", :force => true do |t|
